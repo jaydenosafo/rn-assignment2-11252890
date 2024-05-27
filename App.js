2d3fe6f5b770ed27e51d2d2dd058af4c9b897e9c
jaydenosafo <149180207+jaydenosafo@users.noreply.github.com>
2024-05-27 01:37:09 +0000

    boldened my name

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My name is Jayden</Text>
+      <Text style={styles.text}>My name is <Text style={styles.name}>Jayden</Text></Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,5 +19,8 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 24
+  },
+  name: {
+    fontWeight: 'bold'
   }
 });
